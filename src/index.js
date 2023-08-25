a463e1a1761c1523f278b4d7d26edba5f83635a6
DianaYepes05 <DianaYepes05@users.noreply.github.com>
2023-08-25 12:25:59 +0200

    injected real data from JS
    Added links for moment in index.html in order to call the real data for each city from JS.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+setInterval(() => {
 
+// Los Angeles
+
+let losAngeles = document.querySelector("#los-angeles");
+let losAngelesDate = losAngeles.querySelector(".date");
+let losAngelesTime = losAngeles.querySelector(".time");
+let currentTimeLA = moment().tz("America/Los_Angeles");
+
+losAngelesDate.innerHTML = currentTimeLA.format("MMMM Do YYYY");
+losAngelesTime.innerHTML = `${currentTimeLA.format(
+  'h:mm:ss'
+)} <small>${currentTimeLA.format('A')}</small>`;
+
+
+// Sydney
+
+let sydney = document.querySelector("#sydney");
+let sydneyDate = sydney.querySelector(".date");
+let sydneyime = sydney.querySelector(".time");
+let currentTimeSydney = moment().tz("Australia/Sydney");
+
+sydneyDate.innerHTML = currentTimeSydney.format("MMMM Do YYYY");
+sydneyime.innerHTML = `${currentTimeSydney.format(
+  "h:mm:ss"
+)} <small>${currentTimeSydney.format("A")}</small>`;
+
+  
+}, 1000);
