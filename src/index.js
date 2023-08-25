c888f2c139458e8bd85dbb10da9242ed722c4f06
DianaYepes05 <DianaYepes05@users.noreply.github.com>
2023-08-25 13:20:09 +0200

    made functional the select

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
-setInterval(() => {
+function updateTime() {
+  // Los Angeles
 
-// Los Angeles
+  let losAngeles = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngeles.querySelector(".date");
+  let losAngelesTime = losAngeles.querySelector(".time");
+  let currentTimeLA = moment().tz("America/Los_Angeles");
 
-let losAngeles = document.querySelector("#los-angeles");
-let losAngelesDate = losAngeles.querySelector(".date");
-let losAngelesTime = losAngeles.querySelector(".time");
-let currentTimeLA = moment().tz("America/Los_Angeles");
+  losAngelesDate.innerHTML = currentTimeLA.format("MMMM Do YYYY");
+  losAngelesTime.innerHTML = `${currentTimeLA.format(
+    "h:mm:ss"
+  )} <small>${currentTimeLA.format("A")}</small>`;
 
-losAngelesDate.innerHTML = currentTimeLA.format("MMMM Do YYYY");
-losAngelesTime.innerHTML = `${currentTimeLA.format(
-  'h:mm:ss'
-)} <small>${currentTimeLA.format('A')}</small>`;
+  // Sydney
 
+  let sydney = document.querySelector("#sydney");
+  let sydneyDate = sydney.querySelector(".date");
+  let sydneyime = sydney.querySelector(".time");
+  let currentTimeSydney = moment().tz("Australia/Sydney");
 
-// Sydney
+  sydneyDate.innerHTML = currentTimeSydney.format("MMMM Do YYYY");
+  sydneyime.innerHTML = `${currentTimeSydney.format(
+    "h:mm:ss"
+  )} <small>${currentTimeSydney.format("A")}</small>`;
+}
 
-let sydney = document.querySelector("#sydney");
-let sydneyDate = sydney.querySelector(".date");
-let sydneyime = sydney.querySelector(".time");
-let currentTimeSydney = moment().tz("Australia/Sydney");
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let cities = document.querySelector("#cities");
+  cities.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
 
-sydneyDate.innerHTML = currentTimeSydney.format("MMMM Do YYYY");
-sydneyime.innerHTML = `${currentTimeSydney.format(
-  "h:mm:ss"
-)} <small>${currentTimeSydney.format("A")}</small>`;
+updateTime();
+setInterval(updateTime, 1000);
 
-  
-}, 1000);
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
