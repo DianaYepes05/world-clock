f9b664cc8f178e2e7123f8e0e6830a9d3ca155e0
DianaYepes05 <DianaYepes05@users.noreply.github.com>
2023-08-26 12:05:08 +0200

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 function updateTime() {
   // Los Angeles
-
   let losAngeles = document.querySelector("#los-angeles");
+if (losAngeles){
   let losAngelesDate = losAngeles.querySelector(".date");
   let losAngelesTime = losAngeles.querySelector(".time");
   let currentTimeLA = moment().tz("America/Los_Angeles");
-
+  
   losAngelesDate.innerHTML = currentTimeLA.format("MMMM Do YYYY");
   losAngelesTime.innerHTML = `${currentTimeLA.format(
     "h:mm:ss"
-  )} <small>${currentTimeLA.format("A")}</small>`;
+    )} <small>${currentTimeLA.format("A")}</small>`;
+  }
 
   // Sydney
-
   let sydney = document.querySelector("#sydney");
+if (sydney) {
   let sydneyDate = sydney.querySelector(".date");
   let sydneyime = sydney.querySelector(".time");
   let currentTimeSydney = moment().tz("Australia/Sydney");
-
+  
   sydneyDate.innerHTML = currentTimeSydney.format("MMMM Do YYYY");
   sydneyime.innerHTML = `${currentTimeSydney.format(
     "h:mm:ss"
-  )} <small>${currentTimeSydney.format("A")}</small>`;
+    )} <small>${currentTimeSydney.format("A")}</small>`;
+  }
 }
 
 function updateCity(event) {
